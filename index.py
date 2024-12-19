5048a280ff8697011d9e8fc5a207f0ea07ade673
Gloria Sekyere <gloriasekyere@US-MBP-GSekyere.local>
2024-12-18 20:49:38 -0800

    1. Modify index.py 2. Add training data

--- index.py
+++ index.py
@@ -4,24 +4,28 @@ client = OpenAI()
 
 
 def chat_with_tutor(prompt: str) -> str:
-    """Create conversation with gpt-4o-mini as an Akan Twi tutor."""
+    """Create conversation with gpt-4o-mini as an Asante Twi tutor."""
 
     completion = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
-            {"role": "system", "content": "You are an Akan Twi tutor."},
+            {"role": "system", "content": "You are a native Asante Twi tutor."},
             {"role": "user", "content": prompt},
         ],
-        max_tokens=200,
     )
     return completion.choices[0].message.content.strip()
 
 
 if __name__ == "__main__":
-    print("Akwaaba! Let's learn some Twi today! ('exit' to quit)")
+    print(
+        "Akwaaba! Let's learn how to order food. (Enter 'exit' to quit.)\n"
+        "Example:\n"
+        "\tHow do you ask: Do you sell tomatoes?\n"
+        "\tTranslation: Wo tɔn ntosi?\n"
+    )
     while True:
-        student_input = input("You: ")
-        if student_input.lower() == "exit":
+        question: str = input("How do you ask: ")
+        if question.lower() == "exit":
             break
-        completion = chat_with_tutor(student_input)
-        print(f"Tutor: {completion}")
+        response: str = chat_with_tutor(question)
+        print(f"Translation: {response}")
